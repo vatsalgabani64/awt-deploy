0c715af567912a41ff2d3858639d89f28d7e6f0c
VATSAL GABANI <gabanivatsal17@gmail.com>
2024-04-07 23:05:56 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = reuire('express');
+
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send('Hello World!');
+});
+
+app.listen(5000,() => {
+    console.log("Server running on port 5000");
+})
